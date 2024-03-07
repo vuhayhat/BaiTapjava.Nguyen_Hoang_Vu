81487a844f3c0a11f990eb6018378aefc2a438cb
vuhayhat <142613355+vuhayhat@users.noreply.github.com>
2024-03-07 22:57:43 +0700

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,20 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+package Employee;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+public class Main {
+
+    public static void main(String[] args) {
+        // Tạo đối tượng PartTimeEmployee
+        PartTimeEmployee partTimeEmployee = new PartTimeEmployee("vu", 20, 40);
+
+        // Tạo đối tượng FullTimeEmployee
+        FullTimeEmployee fullTimeEmployee = new FullTimeEmployee("duy", 4000);
+
+        // In ra thông tin nhân viên
+        System.out.println("Nhân viên bán thời gian: " + partTimeEmployee.toString());
+        System.out.println("Lương: " + partTimeEmployee.calculateSalary());
+        System.out.println();
+
+        System.out.println("Nhân viên toàn thời gian: " + fullTimeEmployee.toString());
+        System.out.println("Lương: " + fullTimeEmployee.calculateSalary());
     }
 }
